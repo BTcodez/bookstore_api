4ce6192a4f32d4dffa76caaf9682aae55ce256fe
Brian Tuohy <BTcodez22@gmail.com>
2024-02-07 22:46:04 -0600

    post postman test, routes good

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
   resources :authors, only: [:index, :show, :create, :update, :destroy]
   resources :books, only: [:index, :show, :create, :update, :destroy]
-  get 'author/index'
-  get 'author/show'
-  get 'author/create'
-  get 'author/update'
-  get 'author/destroy'
-  get 'book/index'
-  get 'book/show'
-  get 'book/create'
-  get 'book/update'
-  get 'book/destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
